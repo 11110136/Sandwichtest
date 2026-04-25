3612ab86b1e12044cb72e5ad5e8eea13f4de4788
littlestar <poiu1230051@gmail.com>
2026-04-25 23:57:00 +0800

    feat: 更新 leaveImages 物件，新增空白圖片欄位以擴展功能

--- script.js
+++ script.js
@@ -21,7 +21,15 @@ const leaveImages = {
     1: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?q=80&w=1000&auto=format&fit=crop", 
     2: "./images/level3.png", 
     3: "./images/level4.jpg", 
-    4: "./images/level5.jpg", 5: "", 6: "", 7: "", 8: "", 9: "", 10: "", 11: ""  
+    4: "./images/level5.jpg", 
+    5: "", 
+    6: "", 
+    7: "", 
+    8: "", 
+    9: "", 
+    10: "", 
+    11: "" , 
+    12: "" 
 };
 
 const STATION_IMAGE_URL = "./images/第四版工作分配表.png";
